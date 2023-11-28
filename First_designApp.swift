eb666b8a1ad8b9ba74051254d7426f9ff08ebde9
Shahad Abdullah <139658520+shahad23abdullah@users.noreply.github.com>
2023-11-29 02:56:17 +0300

    Add files via upload

---
+++ First_designApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_designApp.swift
+//  First_design
+//
+//  Created by لمى محمد البرادي  on 14/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct First_designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
